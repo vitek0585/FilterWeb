22a96fcc048fa12fc374d7957497080e2510db83
vitek <vitek0585@mail.ru>
2016-02-29 19:20:58 +0200

    base filters

---
+++ ConditionalFilterGenerator/Abstract/FilterBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Linq.Expressions;
+
+namespace Filters.Abstract
+{
+    public abstract class FilterBase<TItem>
+    {
+        protected NameValueCollection KeyValue;
+
+        protected FilterBase(NameValueCollection keyValue)
+        {
+            KeyValue = keyValue;
+        }
+        public abstract Expression<Func<TItem, TResult>> GetConditional<TResult>();
+    }
+}
